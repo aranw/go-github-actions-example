4ede43f6daadb005095cc8181260d01efd05169d
Aran Wilkinson <aran3001@gmail.com>
2024-02-05 15:46:56 +0000

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aranw/go-github-actions-example
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	msg := sayHello("Aran")
+	fmt.Println(msg)
+}
+
+func sayHello(name string) string {
+	return fmt.Sprintf("Hello %s", name)
+}
