29491b2ade4885e3211c6863c74fdb6ea4756a7a
Aran Wilkinson <aran3001@gmail.com>
2024-02-22 19:30:14 +0000

    handle error for http.ListenAndServe

--- main.go
+++ main.go
@@ -6,9 +6,11 @@ import (
 )
 
 func main() {
-	http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	if err := http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, sayHello("World"))
-	}))
+	})); err != nil {
+		panic(err)
+	}
 }
 
 func sayHello(name string) string {
